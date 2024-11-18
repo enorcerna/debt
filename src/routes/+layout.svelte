c494d275b6a9b61d3075c065c4c08c173a7292a2
Enor Cerna <u17200479@utp.edu.pe>
2024-11-17 23:41:55 -0500

    layout route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '@/styles/app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Deuda</title>
+</svelte:head>
+<main class="flex min-h-screen w-full flex-col items-center justify-center">
+	{@render children()}
+</main>
