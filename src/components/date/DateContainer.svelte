a8a9fe138bc2d868a16ff4500164762c37436584
Enor Cerna <u17200479@utp.edu.pe>
2024-11-17 23:40:29 -0500

    date ui

---
+++ src/components/date/DateContainer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface Props extends HTMLAttributes<HTMLDivElement> {
+		value: number;
+		label?: string;
+	}
+	let { value, label, ...rest }: Props = $props();
+</script>
+
+<div class="flex flex-col items-center gap-1">
+	<span>{label}</span>
+	<div
+		class="bg-card flex aspect-square w-20 items-center justify-center rounded-lg border"
+		{...rest}
+	>
+		<p class="p-2 text-2xl font-bold">
+			{value}
+		</p>
+	</div>
+</div>
